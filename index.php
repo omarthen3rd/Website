a4e3bc77738ae9bc2e736a48c09ad511adbe8316
omarthen3rd <omarehsaan2@gmail.com>
2018-03-02 14:20:06 -0500

    fixed everything

---
+++ index.php
@@ -0,0 +1,45 @@
+<?php include('header.html'); ?>
+
+<!-- TO FIX:
+  <div class="gallery autoplay items-3 p-3">
+    <div id="item-1" class="control-operator"></div>
+    <div id="item-2" class="control-operator"></div>
+    <div id="item-3" class="control-operator"></div>
+    <figure class="item">
+      <img src="assets/1.png">
+    </figure>
+    <figure class="item">
+      <h1>Item 2</h1>
+    </figure>
+    <figure class="item">
+      <h1>Item 3</h1>
+    </figure>
+    <div class="controls"> <a href="#item-1" class="control-button">I</a> <a href="#item-2" class="control-button">I</a> <a href="#item-3" class="control-button">I</a> </div>
+  </div>
+-->
+
+<div class="position-relative overflow-hidden p-3 p-md-5 m-md-3 text-center bg-light">
+  <div class="col-md-5 p-lg-5 mx-auto my-5">
+    <h1 class="display-4 font-weight-normal">coming soon</h1>
+    <p class="lead font-weight-normal">the interactive content slider is causing some issues. please hold a little while longer.</p>
+  </div>
+</div>
+
+<div class="d-md-flex flex-md-equal w-100 my-md-3 pl-md-3">
+  <div class="bg-dark mr-md-3 pt-3 px-3 pt-md-5 px-md-5 text-center text-white overflow-hidden">
+    <div class="my-3 py-3">
+      <h2 class="display-5">Overview</h2>
+      <p class="lead">Subheading.</p>
+    </div>
+    <div class="bg-light box-shadow mx-auto" style="width: 80%; height: 300px; border-radius: 30px 30px 0 0;"></div>
+  </div>
+  <div class="bg-light mr-md-3 pt-3 px-3 pt-md-5 px-md-5 text-center overflow-hidden">
+    <div class="my-3 p-3">
+      <h2 class="display-5">JA Element</h2>
+      <p class="lead">Subheading.</p>
+    </div>
+    <div class="bg-dark box-shadow mx-auto" style="width: 80%; height: 300px; border-radius: 30px 30px 0 0;"></div>
+  </div>
+</div>
+
+<?php include('footer.html'); ?>
